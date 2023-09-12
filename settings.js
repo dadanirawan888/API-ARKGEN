dea69eab87a8ea218a4a6497e50e74248223ac4f
dadanirawan888 <65825785+dadanirawan888@users.noreply.github.com>
2023-09-13 01:09:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'ARK INDO GEN'// yourname
+global.MONGO_DB_URI = "Gendb" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "dadanirawan888@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
